46093de627b0256de43c69f47660248f7a5bb844
christian goddu <rgoddu909@gmail.com>
2023-12-04 21:56:50 -0500

    nice!

--- 02-Challenge/Develop/routes/server.js
+++ 02-Challenge/Develop/routes/server.js
@@ -16,7 +16,36 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
   //write to the db.json file
+
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a review`);
   
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newReview = {
+        product,
+        review,
+        username,
+        upvotes: Math.floor(Math.random() * 100),
+        review_id: uuid(),
+      };
+  
+      const response = {
+        status: 'success',
+        body: newReview,
+      };
+  
+      console.log(response);
+      // res.json() returns data including a status message indicating the success of the request along with the newly created review data.
+      res.status(201).json(response);
+    } else {
+      // the purpose of the else statement is to allow a way to throw an error if either the product, review, or username is not present.
+      res.status(500).json('Error in posting review');
+    }
 });
 
 app.use(express.static('../public'));
